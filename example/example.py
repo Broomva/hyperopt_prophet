6af477d36ad52ba1bee264eb92f614cec7309913
Carlos D. Escobar-Valbuena <carlosdavidescobar@gmail.com>
2023-07-26 17:07:14 -0500

    fix: added compose-dev.yaml, removed pystan from pyproject.toml

--- example/example.py
+++ example/example.py
@@ -3,6 +3,11 @@ import pandas as pd
 
 from hyperopt_prophet.training import ProphetHyperOptTrainer, ProphetTrainingParams
 from hyperopt_prophet.utils import get_plotly_forecast
+import logging
+logger = logging.getLogger('cmdstanpy')
+logger.setLevel(logging.WARNING)
+logging.basicConfig(level=logging.WARNING)
+
 
 # %%
 
@@ -15,5 +20,5 @@ model = ProphetHyperOptTrainer(
 # %%
 prophet_model, model_json, result, avg_metrics, prediction = model.fit()
 # %%
-fig = get_plotly_forecast("WL01", prediction)
+fig = get_plotly_forecast(prophet_model, prediction)
 fig.show()
